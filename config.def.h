edd8c8e7662d6f9d9fbcb0216fdad8665051b1e3
BVK Chaitanya <bvk@bvknano>
2022-12-24 20:31:16 -0800

    Applied maximize patch.

--- config.def.h
+++ config.def.h
@@ -79,6 +79,10 @@ static const Key keys[] = {
 	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
 	{ MODKEY,                       XK_space,  setlayout,      {0} },
 	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
+
+  { MODKEY,                       XK_v,      toggleverticalmax, {0} },
+  { MODKEY,                       XK_x,      togglemaximize, {0} },
+
 	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
 	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
 	{ MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
